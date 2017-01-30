8f03d72a84d687f6d2020ba99f330deca4c3c28b
Luca De Vitis <luca.devitis@moneysupermarket.com>
2017-01-30 22:37:47 +0000

    sorting files

--- lib/msmfg_spec_helper/rake_tasks/module.rb
+++ lib/msmfg_spec_helper/rake_tasks/module.rb
@@ -1,8 +1,10 @@
+require 'msmfg_spec_helper/rake_tasks/helpers'
 require 'msmfg_spec_helper/rake_tasks/module_spec'
 require 'msmfg_spec_helper/rake_tasks/puppet_lint'
 require 'msmfg_spec_helper/rake_tasks/rubocop'
 require 'msmfg_spec_helper/rake_tasks/syntax'
 require 'rake/clean'
+require 'rspec/core/rake_task'
 
 # `:clean` task is supposed to clean intermediate/temporary files
 # `CLEAN` array tells which files to remove on `clean` task.
@@ -11,6 +13,15 @@ CLEAN.include %w(.yardoc coverage log junit)
 # `:clobber` task is uspposed to clean final products. Requires `:clean` task.
 # `CLOBBER` array tells which files to remove on `clobber` task.
 CLOBBER.include %(doc pkg)
+
+desc 'Check the module against MSMFG acceptance specs'
+RSpec::Core::RakeTask.new :module_spec, [:module_path] do |rspec, args|
+  include MSMFGSpecHelper::RakeTasks::Helpers
+  rspec.pattern = File.join(Gem.datadir('msmfg-spec-helper'), 'module_spec.rb')
+  rspec.ruby_opts = "-W0 -C#{module_path(args)}"
+  rspec.rspec_opts = '--color --format documentation'
+  rspec.verbose = false
+end
 
 desc 'Run syntax check, module spec and linters'
 task :validate, [:module_path] => [:syntax,
