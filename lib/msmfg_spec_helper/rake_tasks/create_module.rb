2eb869548c464ff38b1b900d6d5b6689c38766e6
Luca De Vitis <luca.devitis@moneysupermarket.com>
2017-01-31 16:20:44 +0000

    more files handled

--- lib/msmfg_spec_helper/rake_tasks/create_module.rb
+++ lib/msmfg_spec_helper/rake_tasks/create_module.rb
@@ -1,9 +1,11 @@
+# rubocop:disable Metrics/LineLength
 require 'rake'
 
 MODULE_PATH = (ENV['MODULE_PATH'] || '.').freeze
-MODULE_NAME = (ENV['MODULE_NAME'] || ::File.basename(MODULE_PATH)).freeze
+MODULE_NAME = (ENV['MODULE_NAME'] || File.basename(MODULE_PATH)).freeze
+GEM_DATADIR = (ENV['GEM_DATADIR'] || Gem.datadir('msmfg-spec-helper')).freeze
 
-raise "Invalid module name: #{MODULE_NAME}" unless (MODULE_NAME =~ /\w-\w/)
+raise "Invalid module name: #{MODULE_NAME}" unless MODULE_NAME =~ /\w-\w/
 
 DIRECTORIES = [
   "#{MODULE_PATH}/manifests",
@@ -16,7 +18,8 @@ DIRECTORIES = [
   "#{MODULE_PATH}/spec/classes",
   "#{MODULE_PATH}/spec/defines",
   "#{MODULE_PATH}/spec/functions",
-  "#{MODULE_PATH}/spec/types"].freeze
+  "#{MODULE_PATH}/spec/types"
+].freeze
 
 CLASS_NAME = MODULE_NAME.split('-').last.freeze
 
@@ -24,16 +27,16 @@ METADATA = {
   'name' => MODULE_NAME,
   'version' => '0.0.0',
   'author' => 'DevOps Core <devops-core at moneysupermarket.com>',
-  'license' => 'MIT',
+  'license' => 'Private',
   'summary' => '<replace_me>',
   'source' => "https://github.com/MSMFG/#{MODULE_NAME}",
   'project_page' => "https://github.com/MSMFG/#{MODULE_NAME}",
   'issues_url' => "https://github.com/MSMFG/#{MODULE_NAME}/issues",
-  'tags' => [ '<replace_me>' ],
+  'tags' => ['<replace_me>'],
   'operatingsystem_support' => [
     {
       'operatingsystem' => 'CentOS',
-      'operatingsystemrelease' => [ '5.0', '6.0', '7.0' ]
+      'operatingsystemrelease' => ['5.0', '6.0', '7.0']
     }
   ],
   'data_provider' => 'hiera'
@@ -47,40 +50,6 @@ FIXTURE = {
   }
 }.freeze
 
-GEMFILE = <<EOF
-gemspec
-source 'https://rubygems.org' do
-  group :docs do
-    gem 'puppet-strings'
-  end
-
-  group :development do
-    gem 'bundler', '~> 1.11'
-    gem 'coveralls'
-    gem 'hiera'
-    gem 'hiera-puppet-helper'
-    gem 'metadata-json-lint'
-    gem 'pry'
-    gem 'puppet', '< 4'
-    gem 'puppetlabs_spec_helper'
-    gem 'rake', '~> 10.0'
-    gem 'rubocop'
-    gem 'ruby-augeas'
-    gem 'safe_yaml'
-    gem 'serverspec'
-    gem 'wirble'
-  end
-
-  group :acceptance do
-    gem 'beaker'
-    gem 'beaker-facter'
-    gem 'beaker-hiera'
-    gem 'beaker-rspec'
-    gem 'minitest'
-  end
-end
-EOF
-
 NODESET = {
   'HOSTS' => {
     'default' => {
@@ -89,40 +58,104 @@ NODESET = {
       'hypervisor' => 'docker',
       'docker_container_name' => 'centos6.6-msm'
     }
-  }
+  },
   'CONFIG' => {
     'log_level' => 'warn',
     'quite' => 'true',
     'type' => 'foss',
     'masterless' => 'true'
   }
-}
+}.freeze
+
+CLASS_SPEC = <<EOS.freeze
+require 'spec_helper'
+
+describe '#{CLASS_NAME}' do
+  it { is_expected.to compile }
+end
+EOS
+
+ACCEPTANCE_SPEC = <<EOS.freeze
+require 'spec_helper_acceptance'
+
+describe '#{CLASS_NAME} class' do
+  let(:pp) do
+    <<-MANIFEST
+      class { '#{CLASS_NAME}':
+      }
+    MANIFEST
+  end
+
+  describe 'puppet apply manifest' do
+    subject { apply_manifest pp, catch_failures: true }
+
+    it 'should run without errors' do
+      expect(subject.exit_code).to eq 2
+    end
+
+    it 'should run a second time without changes' do
+      expect(subject.exit_code).to eq 0
+    end
+  end
+EOS
 
 DIRECTORIES.each { |path| directory path }
 
-task :skeleton => DIRECTORIES
+task skeleton: DIRECTORIES
 
 file "#{MODULE_PATH}/metadata.json" => :skeleton do |file|
-  ::File.wite(file.name, JSON.pretty_generate(METADATA))
+  File.wite(file.name, JSON.pretty_generate(METADATA))
 end
 
 file "#{MODULE_PATH}/manifests/init.pp" => :skeleton do |file|
-  ::File.write(file.name, "# #{CLASS_NAME}\nclass #{CLASS_NAME} {}")
+  File.write(file.name, "# #{CLASS_NAME}\nclass #{CLASS_NAME} {}")
 end
 
 file "#{MODULE_PATH}/.fixtures.yaml'" => :skeleton do |file|
-  ::File.write(file.name, YAML.dump(FIXTURE))
+  File.write(file.name, YAML.dump(FIXTURE))
+end
+
+file "#{MODULE_PATH}/Rakefile" => :skeleton do |file|
+  rakefile = "#{GEM_DATADIR}/puppet-module/Rakefile"
+  File.write(file.name, File.read(rakefile))
 end
 
 file "#{MODULE_PATH}/Gemfile" => :skeleton do |file|
-  ::File.write(file.name, GEMFILE)
+  gemfile = "#{GEM_DATADIR}/puppet-module/Gemfile"
+  File.write(file.name, File.read(gemfile))
 end
 
 file "#{MODULE_PATH}/Gemfile.lock" => ["#{MODULE_PATH}/Gemfile"] do |file|
+  require 'bundler/cli'
+  require 'bundler/cli/install'
+  require 'bundler/ui'
+  require 'bundler/ui/shell'
+  ENV['BUNDLE_GEMFILE'] = file.soruce
+  Bundler.reset!
+  Bundler.ui = Bundler::UI::Shell.new
+  Bundler::CLI::Install.new('jobs' => 7).run
   # FIXME: use ruby
-  sh "bundle install --jobs=7 --gemfile=#{MODULE_PATH}/Gemfile"
+  # sh "bundle install --jobs=7 --gemfile=#{MODULE_PATH}/Gemfile"
 end
 
-file "#{MODULE_NAME}/acceptance/nodesets/default.yml" => :skeleton do |file|
-  ::File.write(file.name, YAML.dump(NODESET))
+file "#{MODULE_PATH}/spec/spec_helper.rb" => :skeleton do |file|
+  helper = "#{GEM_DATADIR}/puppet-module/specs/spec_helper.rb"
+  File.write(file.name, File.read(helper))
+end
+
+file "#{MODULE_PATH}/spec/classes/#{CLASS_NAME}_spec.rb" => :skeleton do |file|
+  File.write(file.name, CLASS_SPEC)
+end
+
+file "#{MODULE_PATH}/spec/acceptance/nodesets/default.yml" => :skeleton do |file|
+  File.write(file.name, YAML.dump(NODESET))
+end
+
+file "#{MODULE_PATH}/spec/spec_helper_acceptance.rb" => :skeleton do |file|
+  helper = "#{GEM_DATADIR}/puppet-module/specs/spec_helper_acceptance.rb"
+  File.write(file.name, File.read(helper))
+end
+
+file "#{MODULE_PATH}/spec/acceptance/#{CLASS_NAME}_spec.rb" => :skeleton do |file|
+  File.write(file.name, ACCEPTANCE_SPEC)
 end
