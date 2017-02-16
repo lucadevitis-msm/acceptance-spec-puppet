93426de2da086e70e01277513d7431172157104e
Luca De Vitis <luca.devitis@moneysupermarket.com>
2017-02-16 16:24:18 +0000

    Updated to reflect latest policy

--- data/msmfg_spec_helper/msmfg_acceptance_spec.rb
+++ data/msmfg_spec_helper/msmfg_acceptance_spec.rb
@@ -3,12 +3,17 @@
 require 'serverspec'
 require 'json'
 
-if RUBY_PLATFORM =~ /cygwin|mswin|mingw|bccwin|wince|emx/
-  set :backend, :cmd
-  set :os, family: 'windows'
-else
-  set :backend, :exec
-end
+# There is a bug in Ruby which makes it unable to properly manage symbolic
+# links. Otherwise this spec file could work on windows.
+#
+# if RUBY_PLATFORM =~ /cygwin|mswin|mingw|bccwin|wince|emx/
+#   set :backend, :cmd
+#   set :os, family: 'windows'
+# else
+#   set :backend, :exec
+# end
+
+set :backend, :exec
 
 full_module_name = JSON.parse(File.read('metadata.json'))['name']
 
@@ -20,13 +25,14 @@ describe "Puppet module \"#{full_module_name}\"" do
     describe 'metadata' do
       subject { described_class.content_as_json }
       github = 'https://github.com/MSMFG'
+      gh_pages = 'https://msmfg.github.io'
       # should include "version" matching sematic versioning
       it { is_expected.to include('version' => match(/^[0-9]+(\.[0-9]+){0,2}$/)) }
       # should include "author" matching MoneySupermarket.com email
       it { is_expected.to include('author' => match(/at moneysupermarket\.com/)) }
       # should be an MSMFG hosted module
       it { is_expected.to include('source' => match(%r{#{github}/#{full_module_name}})) }
-      it { is_expected.to include('project_page' => match(%r{#{github}/#{full_module_name}})) }
+      it { is_expected.to include('project_page' => match(%r{#{gh_pages}/#{full_module_name}})) }
       it { is_expected.to include('issues_url' => match(%r{#{github}/#{full_module_name}/issues})) }
     end
   end
@@ -55,33 +61,5 @@ describe "Puppet module \"#{full_module_name}\"" do
         is_expected.to include('fixtures' => include('symlinks' => { module_name => '#{source_dir}' }))
       end
     end
-  end
-
-  describe file('spec/acceptance/nodesets/default.yml') do
-    it { is_expected.to be_file }
-    describe 'nodeset' do
-      subject { described_class.content_as_yaml }
-      it 'should configure a masterless environment' do
-        is_expected.to include('CONFIG' => include('masterless' => true))
-      end
-
-      it 'should include a default host' do
-        is_expected.to include('HOSTS' => include('default'))
-      end
-    end
-  end
-
-  describe file('Gemfile') do
-    it { is_expected.to be_file }
-    its(:content) { is_expected.to contain(/gem .msmfg_spec_helper./) }
-  end
-
-  describe file('Gemfile.lock') do
-    it { is_expected.to be_file }
-  end
-
-  describe file('Rakefile') do
-    it { is_expected.to be_file }
-    it { is_expected.to contain(%r{require .msmfg_spec_helper/rake_tasks/puppet_module.}) }
   end
 end
